1b9ae0b37d30c116651aa228a8489c5425bbbd0d
klenivingeri <libertti@live.com>
2025-04-13 19:28:23 -0300

    fix: logo entrada

--- app/UI/WelcomeModal.jsx
+++ app/UI/WelcomeModal.jsx
@@ -7,32 +7,41 @@ export default function WelcomeModal({ open, onClose }) {
   if (!open) return null;
 
   const handleOnClose = () => {
-    //toggleFullScreen()
+    toggleFullScreen()
     resetDistance()
     onClose()
   }
 
   return (
-    <div
-      className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50"
-
-    >
+    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50">
       <div
-        className="backdrop-blur-sm rounded-2xl shadow-lg pb-10 w-80 text-center"
-        onClick={(e) => e.stopPropagation()} // Evita fechar ao clicar no conteúdo
+        className="rounded-2xl shadow-lg pb-10 w-80 text-center relative"
+        onClick={(e) => e.stopPropagation()}
       >
-        <div className='flex flex-1 justify-center'>
-          <img src='/logo.png' height={200} width={200} />
 
+        <div className="flex flex-1 justify-center relative z-50">
+          <img src="/logo.png" height={200} width={200} />
+        </div>
+        <div className="flex flex-1 justify-center absolute left-[0px] top-[-60px] z-30 -scale-x-100">
+          <img src="/diginom/BeelzemonBlastMode.webp" className="-scale-x-100" width={110} />
+        </div>
+        <div className="flex flex-1 justify-center absolute right-[60px] top-[-130px] z-60 ">
+          <img src="/diginom/Cherubimon_29_vg.webp" width={200} />
+        </div>
+
+        <div className="flex flex-1 justify-center absolute right-[-20px] top-[-80px] z-60 -scale-x-100">
+          <img src="/diginom/WarGarurumon.webp" width={180} />
         </div>
 
         <button
           onClick={handleOnClose}
-          className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition"
+          className="bg-gradient-to-b from-yellow-400 to-orange-500 text-white px-6 py-3 rounded-lg hover:brightness-110 transition mt-8 border-b-[6px] border-orange-700 shadow-md font-bold"
         >
           Entrar
         </button>
+
       </div>
     </div>
+
   );
 }
---
+++ app/monsters/monsterMap1.js
@@ -0,0 +1,26 @@
+export const monster = {
+  name: "hahaha",
+  life: 100,
+  nivel: 1,
+  strength: 10,
+  agility: 8,
+  intelligence: 6,
+  speed: 1,
+  critical: 0,
+  defense: 5,
+  attackPower: 12,
+  shadow: {
+    height: 10,
+    width: 25,
+  },
+  sprite: {
+    img: "spriteSrc",
+    height: 25,
+    width: 25,
+    base: 0,
+    rotate: {
+      pet: true,
+      emeny: false,
+    },
+  },
+};
